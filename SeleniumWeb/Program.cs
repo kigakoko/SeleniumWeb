ac4e529ab267a294be7de72f4ad31d82d27afdbf
kigakoko <y.1794.t@gmail.com>
2023-06-05 20:20:59 +0300

    DONE

--- SeleniumWeb/Program.cs
+++ SeleniumWeb/Program.cs
@@ -15,39 +15,38 @@ class Program
 
 
         Console.WriteLine();
-        bool isMenuPresent = driver.PageSource.Contains("menu");
-        bool isBannersPresent = driver.PageSource.Contains("banners");
+        bool menu = driver.PageSource.Contains("menu");
+        bool banner = driver.PageSource.Contains("banners");
         Console.WriteLine("Check a. The main page contains the words 'menu' and 'banners':");
-        Console.WriteLine("Result: " + (isMenuPresent && isBannersPresent));
+        Console.WriteLine("Result: " + (menu && banner));
         Console.WriteLine();
 
 
         string pageSource = driver.PageSource;
         string searchText = "© CoolSoft by Somebody";
-        bool isTextPresent = pageSource.Contains(searchText);
+        bool text = pageSource.Contains(searchText);
         Console.WriteLine("Check b. The lower table cell contains the text 'CoolSoft by Somebody':");
-        Console.WriteLine("Result: " + isTextPresent);
-        Console.WriteLine();
+        Console.WriteLine("Result: " + text);
 
 
-        IWebElement heightInput = driver.FindElement(By.Name("height"));
-        IWebElement weightInput = driver.FindElement(By.Name("weight"));
-        IWebElement nameInput = driver.FindElement(By.Name("name"));
-        IWebElement genderInput = driver.FindElement(By.Name("gender"));
+        IWebElement height = driver.FindElement(By.Name("height"));
+        IWebElement weight = driver.FindElement(By.Name("weight"));
+        IWebElement name = driver.FindElement(By.Name("name"));
+        IWebElement gender = driver.FindElement(By.Name("gender"));
 
 
-        bool areTextFieldsEmpty = string.IsNullOrEmpty(heightInput.GetAttribute("value"))
-            && string.IsNullOrEmpty(weightInput.GetAttribute("value"));
-        bool isGenderNotSelected = !genderInput.Selected;
+        bool areTextFieldsEmpty = string.IsNullOrEmpty(height.GetAttribute("value"))
+            && string.IsNullOrEmpty(weight.GetAttribute("value"));
+        bool isGenderNotSelected = !gender.Selected;
         Console.WriteLine("Check c. By default, all text fields are empty, and the 'Gender' field is not selected:");
         Console.WriteLine("Result: " + (areTextFieldsEmpty && isGenderNotSelected));
         Console.WriteLine();
 
 
-        heightInput.SendKeys("50");
-        weightInput.SendKeys("3");
-        nameInput.SendKeys("Nikita Dubrovin");
-        genderInput.Click();
+        height.SendKeys("50");
+        weight.SendKeys("3");
+        name.SendKeys("Nikita Dubrovin");
+        gender.Click();
         IWebElement submitButton = driver.FindElement(By.XPath("//input[@type='submit']"));
         submitButton.Click();
 
@@ -65,14 +64,14 @@ class Program
             "a group of two radio buttons, and a button:");
         Console.WriteLine("Result: " + isFormPresent);
         driver.Navigate().Refresh();
-        heightInput = driver.FindElement(By.Name("height"));
-        weightInput = driver.FindElement(By.Name("weight"));
+        height = driver.FindElement(By.Name("height"));
+        weight = driver.FindElement(By.Name("weight"));
         submitButton = driver.FindElement(By.XPath("//input[@type='submit']"));
         Console.WriteLine();
 
 
-        heightInput.Clear();
-        weightInput.Clear();
+        height.Clear();
+        weight.Clear();
         submitButton.Click();
         bool isHeightErrorMessageDisplayed = driver.PageSource.Contains("Рост должен быть в диапазоне 50-300 см.");
         bool isWeightErrorMessageDisplayed = driver.PageSource.Contains("Вес должен быть в диапазоне 3-500 кг.");
